3b6c2adbbd76badcda9801d5e7f06c08e989451f
Tess Austin <tessaustin92@gmail.com>
2021-03-23 17:18:53 -0600

    player tests working for stats and inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,9 +1,8 @@
+const { test } = require('@jest/globals');
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
 
 jest.mock('../lib/Potion.js');
-
-console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -15,4 +14,23 @@ test('creates a player object', () => {
 
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)]));
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
